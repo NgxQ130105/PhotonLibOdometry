c05bf6fb38a1b3c643e44acc7ce713f62be18be6
Z3R0O2 <91497379+Z3R0O2@users.noreply.github.com>
2022-02-28 22:11:53 +0700

    Adding Operator Interface

---
+++ OperatorInterface.java
@@ -0,0 +1,33 @@
+package org.photonlib.examples.simposeest.robot;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class OperatorInterface {
+    private XboxController opCtrl = new XboxController(0);
+
+    // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0
+    // to 1.
+    private SlewRateLimiter speedLimiter = new SlewRateLimiter(3);
+    private SlewRateLimiter rotLimiter = new SlewRateLimiter(3);
+
+    public OperatorInterface() {}
+
+    public double getFwdRevSpdCmd() {
+        // Get the x speed. We are inverting this because Xbox controllers return
+        // negative values when we push forward.
+        return -speedLimiter.calculate(opCtrl.getLeftY()) * Constants.kMaxSpeed;
+    }
+
+    public double getRotateSpdCmd() {
+        // Get the rate of angular rotation. We are inverting this because we want a
+        // positive value when we pull to the left (remember, CCW is positive in
+        // mathematics). Xbox controllers return positive values when you pull to
+        // the right by default.
+        return -rotLimiter.calculate(opCtrl.getRightX()) * Constants.kMaxAngularSpeed;
+    }
+
+    public boolean getSimKickCmd() {
+        return opCtrl.getXButtonPressed();
+    }
+}
